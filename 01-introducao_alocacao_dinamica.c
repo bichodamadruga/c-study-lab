164b23e8bbf86178740098cccf53d1c61cd7578d
Felipe Marques da Costa Moreira <sysoutlipewriter@gmail.com>
2026-08-02 23:45:15 -0300

    Add files via upload

---
+++ 01-introducao_alocacao_dinamica.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+
+    // =========================================================================
+    // NOTAS DE ESTUDO: INTRODUÇÃO À ALOCAÇÃO DINÂMICA
+    // =========================================================================
+
+    /*A linguagem C permite alocar(reservar) dinamicamente(em tempo de execução) 
+    blocos de memória utilizando ponteiros. A esse processo dá-se o nome de
+    "Alocação Dinâmica". */
+    
+    /*A linguagem C ANSI usa apenas 4 funções para a Alocação Dinâmica, 
+    disponíveis na biblioteca <stdlib.h>*/
+    //malloc
+    //calloc
+    //realloc
+    //free
+    
+    //Existe também o operador:
+    //sizeof
+    
+    
+
+
+
+	return 0;
+}
